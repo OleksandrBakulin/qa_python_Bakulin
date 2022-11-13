3726d86686e87db22b5230c9df32e354ac9666a7
Oleksandr Bakulin <oleksandr.bakulin@globallogic.com>
2022-11-13 18:18:56 +0200

    Hometask 4

--- forgit.py
+++ forgit.py
@@ -1,4 +1,14 @@
-a = int(input("Enter number #1: "))
-b = int(input("Enter number #2: "))
-print(a + b)
+# status_code = int(input('Enter status code: '))
+# if 100 <= status_code <199:
+#     print("response is Info")
+# if 200 <= status_code <=299:
+#     print("response is OK")
+# if 300 <= status_code <=399:
+#     print("response is redirect")
+# if 400 <= status_code <=499:
+#     print("response is Invalid")
+# if 500 <= status_code <=599:
+#     print("response is server")
+# else:
+#     print("Invalid response")#
 
