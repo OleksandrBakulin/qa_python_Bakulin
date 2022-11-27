080145d82d1802d798cbe5e3b1df01dde0196c21
Oleksandr Bakulin <oleksandr.bakulin@globallogic.com>
2022-11-27 17:14:41 +0200

    Updates

--- forgit.py
+++ forgit.py
@@ -1,14 +1,49 @@
-# status_code = int(input('Enter status code: '))
-# if 100 <= status_code <199:
-#     print("response is Info")
-# if 200 <= status_code <=299:
-#     print("response is OK")
-# if 300 <= status_code <=399:
-#     print("response is redirect")
-# if 400 <= status_code <=499:
-#     print("response is Invalid")
-# if 500 <= status_code <=599:
-#     print("response is server")
+# for i in range(10):
+#     if i > 5 and i % 2 == 0:
+#         break
+#     print(i)
+#     for j in range(101, 105):
+#         print(j)
+#         if j == 102:
+#             break
+#     else:
+#             print("nested for loop")
+#         if not i:
+#             print("start for loop")
 # else:
-#     print("Invalid response")#
+#     print("done")
 
+check_migrated = "Filesystem successfully migrated"
+lst_migrated = check_migrated.split()
+while lst_migrated:
+    removed = lst_migrated.pop()
+    if removed == "successfully":
+        continue
+    print("".join(lst_migrated), removed, sep=";")
+    if removed.lower() == "filesystem":
+        break
+
+else:
+    print("next after")
+
+    # while (number := input("enter a number: ")) and not (number.isdigit() and len(number) ==3):
+    #     print("error mumber")
+    #
+    #
+    # print(f"end | nuber is {number}")
+
+    # count = int(input("enter your number: "))
+    # i = 0
+    # while i < count:
+    #     i += 1
+    #     print(f"iteration N {i}")
+    #     if i % 2 != 0:
+    #         print(f'{i} < {count}')
+    #         continue
+    #     print(i)
+    #
+    # print(f"End")
+
+    for i in range(10):
+        if i % 2 == 0:
+            print(i)
