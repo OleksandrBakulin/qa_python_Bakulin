715b64cd68fbbec4710033edfb4eac41bad3a15a
Oleksandr Bakulin <oleksandr.bakulin@globallogic.com>
2022-11-27 17:15:05 +0200

    Updates

---
+++ HT_4/HT_4_2.py
@@ -0,0 +1,10 @@
+"""
+Пользователь вводит с клавиатуры три числа в переменные a, b, c. Найдите наибольшее число из них и запишите в переменную max.
+Задания со звездочкой:
+"""
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+max = max(a,b,c)
+
+print(max)
