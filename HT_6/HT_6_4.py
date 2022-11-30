a1d5c784848b08c611465b00fbdb2059279a6041
Oleksandr Bakulin <oleksandr.bakulin@globallogic.com>
2022-12-01 00:28:44 +0200

    HT 6

--- HT_6/HT_6_4.py
+++ HT_6/HT_6_4.py
@@ -1,6 +1,6 @@
-# text = input("enter you text here: ")
-# for space in text:
-#     if space.isdigit():
-#         space.replace(space, " ")
-#         print(space)
-#  WORK IN PROGRESS
+text = input("enter you text here: ")
+clear_text = ""
+for digit in text:
+    if digit not in ('0','1','2','3','4','5','6','7','8','9'):
+        clear_text = clear_text + digit
+print(clear_text)
